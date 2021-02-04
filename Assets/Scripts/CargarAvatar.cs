131599997d6c0b5e8a236175b1eb5a9cb8dbccf6
Ayrton Fidel Avalos Cuadrado <70295855+AyrtonFidel9@users.noreply.github.com>
2021-02-03 19:22:15 -0500

    Hecho lo de las escaleras y lo de la eleecion del avatar
    Se realizo las colisiones con los NPCs de las escaleras y tambien la UI de la eleccion del avatar para el juego

---
+++ Assets/Scripts/CargarAvatar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargarAvatar : MonoBehaviour
+{
+    public GameObject hombre;
+    public GameObject mujer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EleccionAvatar eleccion = new EleccionAvatar();
+        cargarAvatar(eleccion.avatar);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void cargarAvatar(int avatar)
+    {
+        if (avatar == 0)
+        {
+            Destroy(mujer);
+        }
+        else if (avatar == 1)
+        {
+            Destroy(hombre);
+        }
+    }
+}
--- Assets/Scripts/CollisionesGradas.cs
+++ Assets/Scripts/CollisionesGradas.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoCamara : MonoBehaviour
+public class CollisionesGradas : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class MovimientoCamara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(-1*Input.GetAxis("Mouse Y"), 0, 0);
+        
     }
+
 }
